dd65ad5f6e6733b550d020275aa60b9358c7f26f
Abhijeet <abhigupta7b@gmail.com>
2023-01-20 14:23:05 +0530

    created a new app accounts and added api(s) for user registration, login, logout and logout all

--- PlacementApi/tpo/models.py
+++ PlacementApi/tpo/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from student.models import Student
 from django.utils import timezone
+
+
 # Create your models here.
 class TPO(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
 
+
 class TPR(models.Model):
-    name = models.ForeignKey(Student,on_delete=models.CASCADE)
+    name = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Announcement(models.Model):
@@ -15,5 +18,5 @@ class Announcement(models.Model):
     type = models.CharField(max_length=100)
     description = models.TextField()
     time = models.DateTimeField(default=timezone.now())
-    tpo = models.ForeignKey(TPO,on_delete=models.CASCADE,null=True)
-    tpr = models.ForeignKey(TPR,on_delete=models.CASCADE,null=True)
+    tpo = models.ForeignKey(TPO, on_delete=models.CASCADE, null=True)
+    tpr = models.ForeignKey(TPR, on_delete=models.CASCADE, null=True)
